e49ecb1803daadb6223985eec64e729780761f55
Başak kılıç <basak.9kilic@gmail.com>
2023-03-28 14:40:43 +0300

    created fields for  elevoter

--- __manifest__.py
+++ __manifest__.py
@@ -32,11 +32,11 @@
         "views/partner_donate_types_views.xml",
         "views/partner_view.xml",
         # "views/partner_website_from_view.xml",
-        "views/partner_boxes.xml",
         "views/partner_provinces.xml",
-        "views/partner_neighbourhoods.xml",
         "views/partner_districts.xml",
-        "views/partner_schools.xml"
+        "views/partner_neighbourhoods.xml",        
+        "views/partner_schools.xml",
+        "views/partner_boxes.xml"
         # "views/partner_blood_groups.xml",
         # "views/partner_education_status_views.xml",
         # "views/partner_driving_license_view.xml",
